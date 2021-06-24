e04ad805b93011099ac17d022bd74e1c369826fc
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-06-24 08:10:52 -0700

    corrected formating and added all time blcoks

---
+++ script.js
@@ -0,0 +1,14 @@
+// shows the current day within the header
+var displayTime = moment().format("MMM Do, YYYY HH:mm");
+$("#currentDay").text(displayTime);
+
+var current = moment.format()
+
+timeBlock = $(".time-block");
+
+console.log(timeBlock);
+
+for(var i = 0; i <= 14; i ++){
+
+
+}
