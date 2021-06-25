642244ccd912d59296e446f6c1f545d24387b519
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-06-24 17:22:40 -0700

    added comments and cleaned up more

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 05-day-planner
+
+// for loop runs from i = 6 to i < 20 to account for variable i that is used to reference each of the text-areas
+// textArea is set to grab everything with an id of each value of i on an iteration through the loop so 
+// it grabs all of textArea html elements which i gave ids of numbers ranging from 6 to 19
+// textArea.val sets the text within that area to whatever is in local storage saved within that save button id
+// the if else statement then check if the id which is also the hour against the current time in military time
+// and assign classes based on that
--- script.js
+++ script.js
@@ -2,44 +2,30 @@
 var displayTime = moment().format("MMM Do, YYYY hh:mm");
 $("#currentDay").text(displayTime);
 
-// logs the current hour in military format to be compared to timeBlock times
+// logs the current hour in military format for easier comparison
 var current = moment().format("k");
-console.log("This is the current time " + current);
 
+// 
 for(var i = 6; i < 20; i ++){
-    // console.log("Should list each time on by one " + timeArray[i]);
-
     var textArea = $("#" + i);
     textArea.val(localStorage.getItem("s" + i));
 
     if(i == current){
 
-        console.log("supposed to be equal" + i + current);
-        console.log("this is the present")
-        // timeSibling[i].nextSibling.addClass("present");
-
         textArea.addClass("present");
 
     } else if (i >= current){
 
-        console.log("left should be greater than" + i + current);
-        console.log("welcome to the future");
-        
         textArea.addClass("future");
-        // timeSibling[i].nextSibling.addClass("future");
 
     } else if (i <= current){
-
-        console.log("left should be less than" + i + current);
-        console.log("past");
         
         textArea.addClass("past");
-        // timeSibling[i].nextSibling.addClass("past");
     };
 
 };
 
-// event listener for save button click
+// event listener for saveBtn click
 $(".saveBtn").click(function() {
    var buttonID = $(this).attr("id");
     var textContent = $(this).siblings("textarea").val();
